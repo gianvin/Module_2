952552d2cea80b8254503036074e774199c547ba
gianvin <ginamangahassanchezong@gmail.com>
2025-09-27 10:49:00 +0800

    edited the todo.App py write functions to add tasks

--- todoApp.py
+++ todoApp.py
@@ -20,3 +20,21 @@ def add_tasks(date, thing_to_do, due, priority, category, remarks):
         "remarks": remarks    
         })
     print("/ Tasks added successfully!")
+#Call the Function for adding tasks
+if __name__ == "__main__":
+    add_tasks(
+        date = "2025-09-27",
+        thing_to_do="Finish Writing the Code for Cloud Database",
+        due="2025-09-28",
+        priority="High",
+        category="School",
+        remarks="Done"
+    )
+# Code for reading tasks
+def get_tasks():
+    tasks = db.collection("tasks").stream()
+    print("\n To-Do List")
+    for task in tasks:
+        data = task.to_dict()
+        print(f" -{data['date']} | {data['thing_to_do']} | Due: {data['due']} | "
+              f"Priority: {data['priority']} | Category: {data['category']} | Reamrks: {data['remarks']}")
